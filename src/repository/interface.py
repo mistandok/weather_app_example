93367111fae42e0d7ef31aff2faa66b8fdc4e6a7
Anton Artikov <arti-anton@yandex.ru>
2024-03-06 23:39:02 +0300

    добавил коммантарии

--- src/repository/interface.py
+++ src/repository/interface.py
@@ -4,9 +4,15 @@ from psycopg2._psycopg import connection
 
 from src.service.model.weather import Weather
 
+# Обычно у всех баз есть какая-то сущность, через которую можно с базой общаться.
+# Используем утиную типизацию питончка, чтобы не мучиться с созданием оберток над разными клиентами.
+# Если надо поддерживать новый тип, то просто нужно добавить его сюда.
 type SupportedDBClient = connection
 
 
+# Соединение передаем в качестве аргумента методов, чтобы поддерживать транзакции,
+# которые могут быть общими для нескольких репозиториев.
+# для транзакций все операции должны выполнятся в одном соединении.
 class RequestsHistoryRepoProtocol(Protocol):
     def save(self, conn: SupportedDBClient, weather: Weather):
         pass
